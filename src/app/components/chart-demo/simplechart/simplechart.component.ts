1eb560a53b8801c0431a24f05a6e883646a90197
edgar <edgar.camara@globalhitss.com.br>
2018-08-23 16:02:16 -0300

    gráfico funcionando

--- src/app/components/chart-demo/simplechart/simplechart.component.ts
+++ src/app/components/chart-demo/simplechart/simplechart.component.ts
@@ -10,23 +10,52 @@ export class SimplechartComponent implements OnInit {
 
   chart = new Chart({
     chart: {
-      type: 'line'
+      type: 'pie',
+        options3d: {
+            enabled: true,
+            alpha: 45,
+            beta: 0
+        }
     },
     title: {
-      text: 'Linechart'
+        text: 'Browser market shares at a specific website, 2014'
     },
-    credits: {
-      enabled: false
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+                enabled: true,
+                format: '{point.name}'
+            }
+        }
     },
     series: [{
-      name: 'Line 1',
-      data: [10, 20, 30, 40, 50]
+        type: 'pie',
+        name: 'Browser share',
+        data: [
+            ['Firefox', 45.0],
+            ['IE', 26.8],
+            {
+                name: 'Chrome',
+                y: 12.8,
+                sliced: true,
+                selected: true
+            },
+            ['Safari', 8.5],
+            ['Opera', 6.2],
+            ['Others', 0.7]
+        ]
     }]
-  });
+});
 
-  add() {
-    this.chart.addPoint(Math.floor(Math.random() * 10));
-  }
+  // add() {
+  //   this.chart.addPoint(Math.floor(Math.random() * 10));
+  // }
 
   constructor() { }
 
